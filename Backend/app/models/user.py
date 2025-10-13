1ce32080ec751ae4ce616f434c68c6ace814ec7b
Jonathan703 <joerey510@gmail.com>
2025-10-13 16:06:09 +0700

    Update user.py

---
+++ Backend/app/models/user.py
@@ -0,0 +1,17 @@
+import datetime
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    Numeric,
+    Enum
+)
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class User(Base):
